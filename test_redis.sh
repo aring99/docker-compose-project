2d8338aa340ac4591cbb29cfcc656bd8b3e39b8f
aring99 <70137414+aring99@users.noreply.github.com>
2025-02-12 11:09:11 +0300

    Update test_redis.sh

--- test_redis.sh
+++ test_redis.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 # Test Redis connection
-REDIS_HOST=redis
-REDIS_PORT=6379
-
-# Test if Redis is responding to a basic command
-redis-cli -p $REDIS_HOST -p $REDIS_PORT PING
+docker exec -it redis_c bash -c "redis-cli PING"
 
